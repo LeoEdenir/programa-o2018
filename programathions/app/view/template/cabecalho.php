0d599c45ef4930828b657e3c74d8ddc5f971ae03
LeoEdenir <leonardo.edenirgraciano@hotmail.com>
2018-04-09 21:06:54 -0300

    Add files via upload

---
+++ programathions/app/view/template/cabecalho.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="style.css">
+<style>
+
+
+</style>
+
+<div class="header">
+    <h1>Lista de Categorias</h1>
+</div>
---
+++ programathions/app/view/template/rodape.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="style.css">
+
+<div class="footer">
+  <p>Gean Carlos|Leonardo Edenir|Tainá Vollmann 3info1</p>
+</div>
